04548eafd7a2b6f3c5741e56824641a501f6f8f2
Rishabh Yadav <1998rishabh31@gmail.com>
2023-08-27 23:58:54 +0530

    Update Is and Is Not Statement.py

--- Basics/Is and Is Not Statement.py
+++ Basics/Is and Is Not Statement.py
@@ -2,15 +2,15 @@ first_number = int(input("Enter first number : "))
 second_number = int(input("Enter second number : "))
 third_number = int(input("Enter second number : "))
 
-#check if both first_number and second_number refer to same value
+#check if both first_number and second_number refer to same memory
 if first_number is second_number:
-    print("Same variable")
+    print("Same memory")
 else:
-    print("Not same variable")
+    print("Not same memory")
     
 
-#check if both first_number and third_number refer to different value
+#check if both first_number and third_number refer to different memory
 if first_number is not third_number:
-    print("Not same variable")
+    print("Not same memory")
 else:
-    print("Same variable")
+    print("Same memory")
