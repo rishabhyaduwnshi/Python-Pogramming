6c65f806ce597427cb9763aeadb3479fcfcf2118
Rishabh Yadav <1998rishabh31@gmail.com>
2023-08-29 00:08:41 +0530

    Create For Loop.py

---
+++ Basics/For Loop.py
@@ -0,0 +1,7 @@
+#factorial of a number
+number = int(input("Enter a number : "))
+factorial = 1;
+for i in range(1,number+1):
+    factorial = factorial*i
+
+print("factorial of number is ",factorial)
