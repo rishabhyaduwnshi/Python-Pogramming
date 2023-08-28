dae09d263863a2416c9da0d3371d12cb8f6f2de9
Rishabh Yadav <1998rishabh31@gmail.com>
2023-08-28 23:38:45 +0530

    Create While Loop.py

---
+++ Basics/While Loop.py
@@ -0,0 +1,9 @@
+number = int(input("Enter a number : "))
+reverse = 0;
+
+while number != 0:
+    remainder = number%10;
+    reverse = reverse*10+remainder
+    number = number//10
+    
+print("Reverse of number is ", reverse)
