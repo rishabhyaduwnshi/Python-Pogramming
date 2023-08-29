8b71a133da23d8a5f59bc26c663466a8ac3b9bba
Rishabh Yadav <1998rishabh31@gmail.com>
2023-08-30 00:08:02 +0530

    Create String Case Methods.py

---
+++ Basics/String Case Methods.py
@@ -0,0 +1,17 @@
+#string methods
+s1 = "hello worLD"
+
+#first word will be in upper case 
+print(s1.capitalize())
+
+#upper case
+print(s1.upper())
+
+#lower case
+print(s1.lower())
+
+#first letter of evry word is capitalised
+print(s1.title())
+
+#swaps the case
+print(s1.swapcase())
