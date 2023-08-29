b192ad5841c624daa3580ee80c685d722f1787fb
Rishabh Yadav <1998rishabh31@gmail.com>
2023-08-30 00:30:41 +0530

    Create String - Replace & Split.py

---
+++ Basics/String - Replace & Split.py
@@ -0,0 +1,15 @@
+string =  "a-b-c-d-e"
+
+#replaces the first string with second string
+print(string.replace("-","."))
+
+s2 = "abc"
+s3 = "xyz"
+
+#performs join
+print(s3.join(s2))
+
+
+s4 = "how-are-you"
+#splits the string based on metioned letter, if no letter is metioned, then sererator is space
+print(s4.split("-"))
