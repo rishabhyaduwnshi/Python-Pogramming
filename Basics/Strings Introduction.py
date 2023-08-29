270d4dbfcbd002b68d7aa94a4dfc5edff8082114
Rishabh Yadav <1998rishabh31@gmail.com>
2023-08-29 07:10:08 +0530

    Create Strings Introduction.py

---
+++ Basics/Strings Introduction.py
@@ -0,0 +1,34 @@
+#string class in Python
+letter = input("Enter a string : ")
+
+#print word by word
+for words in letter:
+    print(words)
+    
+#find length
+print("length of input string is ",len(letter))
+
+#concatenation
+s1 = "Hello"
+s2 = "World"
+s3 = s1+s2
+print(s3)
+
+#typecasting to string
+s4 = "Hello"
+s5 = int("20")
+s6 = s4+str(s5)
+print(s6)
+
+#multiplication
+s7 = "Hi"
+print(s7*3)
+
+
+
+
+
+
+
+
+
