97a2875d2773bfa88b1e62814164e68d06c3f9bd
Rishabh Yadav <1998rishabh31@gmail.com>
2023-08-30 00:15:10 +0530

    Create String Enquiry Methods 1.py

---
+++ Basics/String Enquiry Methods 1.py
@@ -0,0 +1,20 @@
+#string methods
+s1 = "HELLO"
+s2 = "hello"
+s3 = "How Are You"
+s4 = "hello1"
+
+#retuns true if all letters are in upper case
+print(s1.isupper())
+
+#retuns true if all letters are in lower case
+print(s2.islower())
+
+#if all letters of every word if uppercase
+print(s3.istitle())
+
+#checks if string contains alphanumeric characters
+print(s4.isalnum())
+
+#checks if string contains only alphabets
+print(s1.isalpha())
