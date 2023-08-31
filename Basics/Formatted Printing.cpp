9fc09aeac3dd7a2447223db7bca31786b66893da
Rishabh Yadav <1998rishabh31@gmail.com>
2023-09-01 00:01:26 +0530

    Create Formatted Printing.cpp

---
+++ Basics/Formatted Printing.cpp
@@ -0,0 +1,16 @@
+#prints the ascii value of A
+x = 'A'
+print(ord(x))
+
+#prints the character value of ascii code 65
+y = 65
+print(chr(y))
+
+
+#formatted prinitng
+name = "Rishabh"
+age = 25;
+company = "Netscout"
+
+
+print("My name is %s and age is %d and working for %s "%(name,age,company))
