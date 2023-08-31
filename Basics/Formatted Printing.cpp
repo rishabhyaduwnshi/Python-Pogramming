97d49c6ea722b4d8086a0b06be3b4773b798decc
Rishabh Yadav <1998rishabh31@gmail.com>
2023-09-01 00:03:57 +0530

    Update Formatted Printing.cpp

--- Basics/Formatted Printing.cpp
+++ Basics/Formatted Printing.cpp
@@ -11,6 +11,10 @@ print(chr(y))
 name = "Rishabh"
 age = 25;
 company = "Netscout"
-
-
 print("My name is %s and age is %d and working for %s "%(name,age,company))
+
+
+number1 = 10
+number2 = 20
+
+print("Sum of {} and {} is {}".format(number1,number2,number2+number1))
