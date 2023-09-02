3f4a4094cc1dd96b6a96764cf7c101667ad9b942
Rishabh Yadav <1998rishabh31@gmail.com>
2023-09-02 13:36:04 +0530

    Create Introduction to Lists.py

---
+++ Lists/Introduction to Lists.py
@@ -0,0 +1,24 @@
+my_list = ["rishabh","aditya","shreya","aastha","amisha"]
+
+#print the entire list
+print(my_list)
+
+#copy list from other list
+my_list1 = my_list
+print(my_list1)
+
+#print any element at index 2 from list
+print(my_list[2])
+
+#list is heterogenous
+my_list2 = ["rishabh",1,2,3,4.5]
+print(my_list2)
+
+#lists are mutable
+my_list[1] = "lucky"
+print(my_list)
+
+#add an element to end of list
+my_list.append("shivam")
+print(my_list)
+
