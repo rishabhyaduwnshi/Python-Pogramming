fef4ae4abf9cca2db468712c5e004f580327dde6
Rishabh Yadav <1998rishabh31@gmail.com>
2023-09-02 13:55:30 +0530

    Create Loops on Lists.py

---
+++ Lists/Loops on Lists.py
@@ -0,0 +1,17 @@
+#iteration of list
+
+#print all elements one by one 
+my_list = [1,2,3,4,5,6,7,8,9,10]
+for i in my_list:
+    print(i)
+    
+#printing based on index
+for i in range(len(my_list)):
+    print(my_list[i])
+    
+
+#printing using while loop
+i = 0
+while i < len(my_list):
+    print(my_list[i])
+    i +=1
