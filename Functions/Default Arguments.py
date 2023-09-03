8a5a37ccc4251608662fed46f1638cd39ece79ab
Rishabh Yadav <1998rishabh31@gmail.com>
2023-09-03 10:43:03 +0530

    Create Default Arguments.py

---
+++ Functions/Default Arguments.py
@@ -0,0 +1,14 @@
+#functions indtroduction
+
+#default 
+#default arguments are created only once
+
+def add_two_number(number1=0, number2=0,number3=0):
+    result = number1+number2+number3
+    return result
+
+result = add_two_number(10,20)
+print("Sum of two numbers is: ", result)
+
+result = add_two_number(10,20,30)
+print("Sum of three numbers is: ", result)
