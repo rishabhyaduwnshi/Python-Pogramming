e954459f011cdd5ddbc314d6f559fd47d3c171a3
Rishabh Yadav <1998rishabh31@gmail.com>
2023-09-03 12:31:23 +0530

    Create Iterator.py

---
+++ Functions/Iterator.py
@@ -0,0 +1,10 @@
+#iter works for every sequence
+
+my_list = [1,2,3,4,5]
+iterator = iter(my_list)
+
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
