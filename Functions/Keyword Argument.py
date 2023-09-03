add133fc38faa9c9a271caed8c8a79efa512160c
Rishabh Yadav <1998rishabh31@gmail.com>
2023-09-03 10:31:00 +0530

    Update Keyword Argument.py

--- Functions/Keyword Argument.py
+++ Functions/Keyword Argument.py
@@ -4,5 +4,6 @@ def add_two_number(number1, number2):
     result = number1+number2
     return result
 
+#parametrs name should be same as defined in function
 result = add_two_number(number2=100,number1=300)
 print("Result is : ", result)
