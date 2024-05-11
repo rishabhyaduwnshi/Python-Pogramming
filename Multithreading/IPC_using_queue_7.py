8cbd78beaafd24ac9200541f0189e1f4bd24dc85
Rishabh Yadav <1998rishabh31@gmail.com>
2024-05-11 16:32:00 +0530

    Create IPC_using_queue_7.py

---
+++ Multithreading/IPC_using_queue_7.py
@@ -0,0 +1,30 @@
+from threading import *
+from time import *
+from queue import *
+
+def producer(queue):
+    i = 0
+    while True:
+        queue.put(i)
+        print("Produer : ",i)
+        sleep(0.2)
+        i += 1
+
+def consumer(queue):
+    while True:
+        x = queue.get()
+        print("Consumer : ", x)
+        sleep(0.2)
+
+q = Queue()
+
+t1 = Thread(target = lambda:producer(q))
+t2 = Thread(target = lambda:consumer(q))
+
+t1.start()
+t2.start()
+
+t1.join()
+t2.join()
+
+
