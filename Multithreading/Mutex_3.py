6fba4110f9a93a6b1fa1ea76f5fe7a52df5c830b
Rishabh Yadav <1998rishabh31@gmail.com>
2024-05-11 12:49:24 +0530

    Create Mutex_3.py

---
+++ Multithreading/Mutex_3.py
@@ -0,0 +1,20 @@
+from threading import *
+from time import *
+
+def display(string):
+    l.acquire()
+    for x in string:
+        print(x)
+    l.release()
+
+
+t1 = Thread(target=display,args=("HELLO",))
+t2 = Thread(target=display,args=("welcome",))
+
+l = Lock()
+
+t1.start()
+t2.start()
+
+t1.join()
+t2.join()
